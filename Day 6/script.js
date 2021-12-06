2894b9ffeba41b77b53fa03d3c29409bafdc2ddf
Sorin <jordache_sor@yahoo.com>
2021-12-06 11:46:34 +0200

    Update script.js

--- Day 6/script.js
+++ Day 6/script.js
@@ -7,48 +7,40 @@ var timer = null
 var seconds
 
 function christmassifyName() {
-   // Task:
-  // - Add christmassify class to greeting.
-  //greeting.classList.add("christmassified")
-  //- Check whether christmassifierBtn has christmassify on it. If so, change text to "De-christmassify", if not, change text to "Christmassify"
-  if (christmassifierBtn.innerText == "Christmassify")
-  {
-    greeting.classList.add("christmassified")
-    AddMusic()
-    seconds = 20
-    christmassifierBtn.innerText = `De-christmassify (${seconds})`
-    timer = setInterval(UpdateTimer, 1000)
-  }
-  else
-  {
-    greeting.classList.remove("christmassified")
-    RemoveMusic()
-    clearTimeout(timer)
-    christmassifierBtn.innerText = "Christmassify"
-  }
+    // Task:
+    // - Add christmassify class to greeting.
+    //greeting.classList.add("christmassified")
+    //- Check whether christmassifierBtn has christmassify on it. If so, change text to "De-christmassify", if not, change text to "Christmassify"
+    if (christmassifierBtn.innerText == "Christmassify") {
+        greeting.classList.add("christmassified")
+        AddMusic()
+        seconds = 20
+        christmassifierBtn.innerText = `De-christmassify (${seconds})`
+        timer = setInterval(UpdateTimer, 1000)
+    } else {
+        greeting.classList.remove("christmassified")
+        RemoveMusic()
+        clearTimeout(timer)
+        christmassifierBtn.innerText = "Christmassify"
+    }
 }
 
 // Stretch goals:
 // - Play Christmas music when Christmas class is added.
-function AddMusic()
-{
+function AddMusic() {
     music.innerHTML = '<iframe src="https://open.spotify.com/embed/track/5hslUAKq9I9CG2bAulFkHN?utm_source=generator&theme=0" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>'
 }
 
-function RemoveMusic()
-{
+function RemoveMusic() {
     music.innerHTML = ""
 }
+
 // - Remove the Christmas class after a given time.
-function UpdateTimer()
-{
+function UpdateTimer() {
     seconds--
-    if (seconds > 0)
-    {
+    if (seconds > 0) {
         christmassifierBtn.innerText = `De-christmassify (${seconds})`
-    }
-    else
-    {
+    } else {
         clearTimeout(timer)
         christmassifyName()
     }
