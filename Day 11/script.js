06430f240b7eff80ab86c57d2482bc3eab851101
Sorin <jordache_sor@yahoo.com>
2021-12-11 12:06:22 +0200

    fixed formatting

--- Day 11/script.js
+++ Day 11/script.js
@@ -1,13 +1,19 @@
 const previous = document.getElementById("previous")
 const next = document.getElementById("next")
 const img = document.getElementById("img")
-const imgs = [
-    {src: "imgs/village.jpg",
-    alt: "christmas village at night with snow and christmas tree"},
-    {src: "imgs/present.jpg",
-    alt: "white and gold wrapped present on white table with snowflake decorations"},
-    {src: "imgs/doggies.jpg",
-    alt: "small black dog and small beige dog looking out the window at snow next to Christmas wreath"}]
+const imgs = [{
+        src: "imgs/village.jpg",
+        alt: "christmas village at night with snow and christmas tree"
+    },
+    {
+        src: "imgs/present.jpg",
+        alt: "white and gold wrapped present on white table with snowflake decorations"
+    },
+    {
+        src: "imgs/doggies.jpg",
+        alt: "small black dog and small beige dog looking out the window at snow next to Christmas wreath"
+    }
+]
 
 let imgNum = 0
 ShowImage()
@@ -33,17 +39,15 @@ function ShowPrevImage() {
 
 function GetNextIndex() {
     imgNum++
-    if (imgNum == imgs.length)
-    {
+    if (imgNum == imgs.length) {
         imgNum = 0
     }
 }
 
 function GetPrevIndex() {
     imgNum--
-    if (imgNum < 0)
-    {
-        imgNum = imgs.length-1
+    if (imgNum < 0) {
+        imgNum = imgs.length - 1
     }
 }
 // - Make sure that the gallery works no matter how many images are added.
