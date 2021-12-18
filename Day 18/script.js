e356193b0e4fa8245d4f42ff4bf3a4ef2d98760e
Sorin <jordache_sor@yahoo.com>
2021-12-18 15:23:01 +0200

    completed day 18

--- Day 18/script.js
+++ Day 18/script.js
@@ -1,39 +1,80 @@
-const items = ["Candles", "Decorations", "Chocolate"]
+const items = ["Candles", "Decorations", "Chocolate", "Socks", "Cookies"]
 const checklist = document.getElementById("checklist")
+const checks = ["present", "candles", "decorations", "chocolate", "cookies", "socks"]
+const randomChecks = ["candles", "decorations", "chocolate", "santa", "mrs-claus", "elf", "deer", "snowflake", "snowman", "cookies", "bell"]
 
-for (let i=0; i<items.length; i++) {
-    checklist.innerHTML += `
-         <div class="checklist-item">
+const presentBtn = document.getElementById("preset-btn")
+const randomBtn = document.getElementById("random-btn")
+const matchingBtn = document.getElementById("matching-btn")
+
+presentBtn.addEventListener("click", AssignPresentChecks)
+randomBtn.addEventListener("click", AssignRandomChecks)
+matchingBtn.addEventListener("click", AssignMatchingChecks)
+
+function GetRandomIndex(array) {
+    return Math.floor(Math.random() * array.length)
+}
+
+function GetRandomCheck(array) {
+    let index = GetRandomIndex(array)
+    let randomCheck = array[index]
+    if (index > -1) {
+        array.splice(index, 1)
+    }
+    return randomCheck
+}
+
+function AssignRandomChecks() {
+    checklist.innerHTML = ""
+    let array = [...randomChecks]
+
+    for (let i = 0; i < items.length; i++) {
+        let check = GetRandomCheck(array)
+        checklist.innerHTML += `
+            <div class="checklist-item">
                 <input type="checkbox" id="${items[i]}" value="${items[i]}">
-                <label for="${items[i]}" class="strikethrough">
+                <label for="${items[i]}" class="strikethrough ${check}">
                     <span class="custom-checkbox">${items[i]}
                 </label>
             </div>
-    `
+        `
+    }
 }
 
-checklist.innerHTML += `
-<div class="checklist-item">
-<input type="checkbox" id="Candles2" value="Candles2" class="candles">
-<label for="Candles2" class="strikethrough">
-    <span class="custom-checkbox">Candles
-</span></label>
-</div>
-    `
+function AssignPresentChecks() {
+    checklist.innerHTML = ""
+    for (let i = 0; i < items.length; i++) {
+        checklist.innerHTML += `
+            <div class="checklist-item">
+                <input type="checkbox" id="${items[i]}" value="${items[i]}">
+                <label for="${items[i]}" class="strikethrough present">
+                    <span class="custom-checkbox">${items[i]}
+                </label>
+            </div>
+        `
+    }
+}
 
-    checklist.innerHTML += `
-    <div class="checklist-item">
-    <input type="checkbox" id="Decorations2" value="Decorations" class="decorations">
-    <label for="Decorations2" class="strikethrough">
-        <span class="custom-checkbox">Decorations
-    </span></label>
-</div>
-    `
+function AssignMatchingChecks() {
+    checklist.innerHTML = ""
+    for (let i = 0; i < items.length; i++) {
+        let itemValue = items[i].toLowerCase()
+        let check = GetMatchingCheck(itemValue)
+        checklist.innerHTML += `
+            <div class="checklist-item">
+                <input type="checkbox" id="${itemValue}" value="${items[i]}">
+                <label for="${itemValue}" class="strikethrough ${check}">
+                    <span class="custom-checkbox">${items[i]}
+                </label>
+            </div>
+        `
+    }
+}
 
-    checklist.innerHTML += `
-    <div class="checklist-item">
-    <input type="checkbox" id="Chocolate2" value="Chocolate" class="chocolate">
-    <label for="Chocolate2" class="strikethrough">
-        <span class="custom-checkbox">Chocolate
-    </span></label>
-</div>`
+function GetMatchingCheck(value) {
+    if (checks.includes(value)) {
+        return value
+    } else {
+        return "present"
+    }
+}
